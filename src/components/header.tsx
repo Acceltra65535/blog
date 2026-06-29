eab76b279b056d09471b037ee6827d2b2f859c68
Acceltra65535 <work@comm.moe>
2026-06-28 17:27:00 -0700

    Add header feature and svg file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,18 @@ import { Link } from 'waku';
 
 export const Header = () => {
   return (
-      <header className="flex w-full items-center p-6">
+      <header className="flex w-full flex-col items-start p-6">
           <h2 className="text-3xl font-bold tracking-tight">
               <Link to="/">Alex Yuan</Link>
           </h2>
+          <nav className="mt-[20px]">
+              <a href="/" className="mr-[20px]">
+                  Home
+              </a>
+              <a href="/blog" className="mr-[20px]">
+                  博客
+              </a>
+          </nav>
       </header>
   );
 };
