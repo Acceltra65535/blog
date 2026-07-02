898441b36721939a33b8b6d177f161f16a89faed
Acceltra65535 <work@comm.moe>
2026-07-01 22:27:20 -0700

    Updated index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,13 @@ export default async function HomePage() {
       <p className="mt-2">Blood of Elves/精灵之血, 猎魔人卷三</p>
       <p className="mt-2">Time of Contempt/轻蔑时代, 猎魔人卷四</p>
       <p className="mt-2">Baptism of Fire/火之洗礼, 猎魔人卷五</p>
+      <p className="mt-2">The Tower of the Swallow/雨燕之塔, 猎魔人卷六</p>
+      <p className="mt-2">The Lady of the Lake/湖中女士, 猎魔人卷七</p>
+      <p className="mt-2">Season of Storms/風暴季節, 猎魔人卷八</p>
+      <p className="mt-2">Neuromancer/神经漫游者</p>
+      <p className="mt-2">Is-slottet/冰宫</p>
+      <p className="mt-2">The Ghost in the Shell/攻殻機動隊</p>
+      <p className="mt-2">THuman-Error Processor/攻殻機動隊1.5</p>
 
       <h3 className="mt-5 text-2xl font-bold text-[#103222]">Movies/Shows</h3>
       <p className="mt-2">The Expanse/苍穹浩瀚 2015–2022</p>
@@ -24,6 +31,8 @@ export default async function HomePage() {
       <p className="mt-2">Dark/闇 2017–2020</p>
       <p className="mt-2">The Walking Dead/行尸走肉 2010–2022</p>
       <p className="mt-2">The Last Ship/末日孤艦 2014–2018</p>
+      <p className="mt-2">Ghost in the Shell/攻殻機動隊 1995</p>
+      <p className="mt-2">Blade Runner 2049/银翼杀手2049 2017</p>
 
       <h3 className="mt-5 text-2xl font-bold text-[#103222]">Games</h3>
       <p className="mt-2">Battlefield 3/戰地風雲3 2011</p>
@@ -31,6 +40,8 @@ export default async function HomePage() {
       <p className="mt-2">Grand Theft Auto V/侠盗猎车手5 2013</p>
       <p className="mt-2">Tom Clancy's Rainbow Six Siege/彩虹六号: 围攻 2015</p>
       <p className="mt-2">The Witcher 3: Wild Hunt/巫师3: 狂猎 2015</p>
+      <p className="mt-2">Subnautica/深海迷航 2018</p>
+      <p className="mt-2">Subnautica: Below Zero/深海迷航: 冰点之下 2021</p>
 
       <h3 className="mt-5 text-2xl font-bold text-[#103222]">Publications</h3>
       <p className="mt-2">Not yet.</p>
